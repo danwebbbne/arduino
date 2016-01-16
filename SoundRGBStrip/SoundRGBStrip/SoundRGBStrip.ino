37c9c8eeb7005d15b9c2c37495cce967023abe24
danwebbbne <daniel.webb@rslqld.org>
2016-01-16 14:53:57 +1000

    Adding variable fadeFrequency and fadeSpeed (read from Pot)

--- SoundRGBStrip/SoundRGBStrip/SoundRGBStrip.ino
+++ SoundRGBStrip/SoundRGBStrip/SoundRGBStrip.ino
@@ -8,8 +8,8 @@
 #define SOUNDPIN 0
 #define FADESPEED 5     // make this higher to slow down
 const int sampleWindow = 50;
-const int fadeFrequency = 30000;
-const int fadeSpeed = 500;
+unsigned int fadeFrequency = 60000;
+int fadeSpeed = 20;
 unsigned int sample;
 unsigned long previousMillisInitFade = 0;
 unsigned long previousMillisFade = 0;
@@ -20,6 +20,8 @@ int bValue = 0;
 int rValueTarget = 0;
 int gValueTarget = 0;
 int bValueTarget = 0;
+int fadeFrequencyPotPin = A1;
+int fadeSpeedPotPin = A2;
 
 void setup() {
   pinMode(REDPIN, OUTPUT);
@@ -43,6 +45,9 @@ void loop() {
   unsigned int signalMax = 0;
   unsigned int signalMin = 1024;
 
+  fadeFrequency = analogRead(fadeFrequencyPotPin) * 120;
+//  fadeSpeed = analogRead(fadeSpeedPotPin) / 2;
+  
   while (millis() - startMillis < sampleWindow)
   {
     sample = analogRead(SOUNDPIN);
@@ -59,40 +64,48 @@ void loop() {
     }
   }
   peakToPeak = signalMax - signalMin;
-  brightness = (constrain(peakToPeak,0,192));
+  brightness = (constrain(peakToPeak,0,64));
   setRGB(rValue,gValue,bValue,brightness);
 
-  if (startMillis - previousMillisInitFade >= fadeFrequency){
-    previousMillisInitFade = startMillis;
-    Serial.println("Fading");
-    rValueTarget = random(0,64);
-    gValueTarget = random(0,64);
-    bValueTarget = random(0,64);
 
-  Serial.print("Writing red: "); Serial.println(rValueTarget);
-  Serial.print("Writing green:" ); Serial.println(gValueTarget);
-  Serial.print("Writing blue: "); Serial.println(bValueTarget);
+  if (startMillis - previousMillisInitFade >= fadeFrequency){
+    if (fadeFrequency != 0){
+      previousMillisInitFade = startMillis;
+      Serial.println("Fading");
+      Serial.println(fadeFrequency);
+      Serial.println(analogRead(fadeFrequencyPotPin));
+      rValueTarget = random(0,192);
+      gValueTarget = random(0,192);
+      bValueTarget = random(0,192);
+    }
   }
 
   if (startMillis - previousMillisFade >= fadeSpeed){
     previousMillisFade = startMillis;
+    
     if(rValue < rValueTarget){
       rValue++;
+      Serial.print("Red: ");Serial.println(rValue);
     }
     if(rValue > rValueTarget){
       rValue--;
+      Serial.print("Red: ");Serial.println(rValue);
     }
     if(gValue < gValueTarget){
       gValue++;
+      Serial.print("Green: ");Serial.println(gValue);
     }
     if(gValue > gValueTarget){
       gValue--;
+      Serial.print("Green: ");Serial.println(gValue);
     }
     if(bValue < bValueTarget){
       bValue++;
+      Serial.print("Blue: ");Serial.println(bValue);
     }
     if(bValue > bValueTarget){
       bValue--;
+      Serial.print("Blue: ");Serial.println(bValue);
     }
   }
 
